2429dfc5a04d3743a1cd6c7a12f43a2a904036cf
maslukasz <lukesekmc@gmail.com>
2022-11-08 19:06:14 +0100

    kolejny fix work

--- src/economy/earnings.py
+++ src/economy/earnings.py
@@ -17,7 +17,7 @@ async def work(ctx: lightbulb.Context) -> None:
     r = await c.fetchone()
 
     if r is None:
-        await c.execute(f"INSERT INTO cooldowns (userid, work) VALUES ({ctx.author.id}, date(now())")
+        await c.execute(f"INSERT INTO cooldowns (userid, work) VALUES ({ctx.author.id}, timestamp(now()))")
         await ctx.respond(hikari.Embed(description=f'<:nie:783659534455275560> Nie było Cię w bazie danych. Spróbuj jeszcze raz.', color='#ff0000'))
         return
 
@@ -36,6 +36,7 @@ async def work(ctx: lightbulb.Context) -> None:
         new_money = r[0] + earn
 
         await c.execute(f"UPDATE userdata SET money = {new_money} WHERE userid = {ctx.author.id}")
+        await c.execute(f"UPDATE cooldowns SET work = date_add(now(), interval 2 hour) WHERE userid = {ctx.author.id}")
         work_reply = [f'Skomponowałeś/aś utwór do gry. Deweloper dał Ci **{earn}** <:thend:742800976636936202> wynagrodzenia.', f'Ładnie odkurzyłeś/-aś pokój. Mama dała Ci za to **{earn}** <:thend:742800976636936202>.', f'Pomogłeś/-aś starszej Pani przejść przez pasy. Dała Ci za pomoc **{earn}** <:thend:742800976636936202>.', f'Zgłosiłeś delikwenta, który reklamował się w prywatnej wiadomości. Administracja dała Ci za to **{earn}** <:thend:742800976636936202>.', f"Założyłeś się ze znajomymi, że uda Ci się ugrać clutch'a 1 na 5 w csie. Dali Ci za to **{earn}** <:thend:742800976636936202>."]
         await ctx.respond(hikari.Embed(description=random.choice(work_reply), colour='#4F545C'))
         
